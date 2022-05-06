a3ed5c94819c63fc008f1f55a62fb3956bca6eb1
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-05-06 08:35:53 +0200

    update array in script.js

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+let pokemonList = [{name:'Bulbasaur', height: 7, types:['grass', 'poison']},
+                    {name:'Ivysaur', height: 1, types:['grass', 'poison']},
+                    {name: 'Venusaur', height: 2, types: ['grass', 'poison']}
+];

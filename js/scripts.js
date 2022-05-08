0de31a8823298d8c8d0db2b01ed8389e6a024b4d
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-05-08 08:08:17 +0200

    added iteration to script.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,16 @@ let pokemonList = [{name:'Bulbasaur', height: 7, types:['grass', 'poison']},
                     {name:'Ivysaur', height: 1, types:['grass', 'poison']},
                     {name: 'Venusaur', height: 2, types: ['grass', 'poison']}
 ];
+
+// iterate over the list of objects
+// checking for the height of a specific object
+// writing name and height onto the doc
+for (let i=0; i<pokemonList.length; i++) {
+  if (pokemonList[i].height >= 5) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!');
+    document.write('<br>');
+  } else {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    document.write('<br>');
+  }
+}

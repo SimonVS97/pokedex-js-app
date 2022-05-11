1b4a0bf99478cf9fa6c51868954b9a0af37cd425
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-05-11 07:54:00 +0200

    turned for into forEach loop script.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,14 @@ let pokemonList = [{name:'Bulbasaur', height: 7, types:['grass', 'poison']},
 // iterate over the list of objects
 // checking for the height of a specific object
 // writing name and height onto the doc
-for (let i=0; i<pokemonList.length; i++) {
-  if (pokemonList[i].height >= 5) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big!');
+pokemonList.forEach(function(pokemon) {
+  if(pokemon.height >= 5) {
+    document.write(pokemon.name + ' (height: ' + pokemon.height + ')' + ' - Wow, that\'s big!');
     document.write('<br>');
   } else {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
-    document.write('<br>');
+    document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
+    document.write('<br>')
   }
-}
+});
+
+

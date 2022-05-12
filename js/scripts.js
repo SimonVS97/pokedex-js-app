2645aa95cbddad7fa8e3e4c52a100760e06f0ca1
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-05-12 07:42:22 +0200

    wrapped array in IIFE srcript.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
-let pokemonList = [{name:'Bulbasaur', height: 7, types:['grass', 'poison']},
+// IIFE to wrap array
+
+let pokemonRespository = (function () {
+  let pokemonList = [{name:'Bulbasaur', height: 7, types:['grass', 'poison']},
                     {name:'Ivysaur', height: 1, types:['grass', 'poison']},
                     {name: 'Venusaur', height: 2, types: ['grass', 'poison']}
-];
+  ];
+  function getAll() {
+    return pokemonList;
+  }
 
+  function add(pokemon) {
+    return pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+
+})();
+
+// Retrieve array from IIFE
 // iterate over the list of objects
 // checking for the height of a specific object
 // writing name and height onto the doc
-pokemonList.forEach(function(pokemon) {
+pokemonRespository.getAll().forEach(function(pokemon) {
   if(pokemon.height >= 5) {
     document.write(pokemon.name + ' (height: ' + pokemon.height + ')' + ' - Wow, that\'s big!');
     document.write('<br>');
